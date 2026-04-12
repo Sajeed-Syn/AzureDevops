69273e231999bd8e13beeb099ac24e6e0894a952
Sajeed <sajeedrs@gmail.com>
2026-04-12 19:57:17 +0530

    saved

---
+++ calc/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Add2Nos(10, 20));
+
+int Add2Nos(int a, int b)
+{
+    return a + b;
+}
+
---
+++ calctest/UnitTest1.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+
+namespace calctest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        int a = 10;
+        int b = 20;
+        int expected = 30;
+
+        // Act
+        int result = Add2Nos(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        int a = -5;
+        int b = 15;
+        int expected = 20;
+
+        // Act
+        int result = Add2Nos(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
